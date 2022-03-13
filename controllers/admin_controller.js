610841f5e26cc4757046d4d1484459942e12daca
Ryan Maloney <rypmaloney@gmail.com>
2022-03-13 10:44:52 -0400

    Update post_update_post. removed function from around it.

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -21,44 +21,43 @@ exports.get_all_posts = (req, res) => {
 };
 
 /* POST new post function for /posts/new/ */
-(exports.post_new_post = passport.authenticate('jwt', { session: false })),
-    (req, res) => [
-        // Validate and sanitize fields.
-        body('title', 'Title must not be empty.').isLength({ min: 1 }),
-        body('body', 'The actually post is required, buddy')
-            .trim()
-            .isLength({ min: 1 }),
+exports.post_new_post = [
+    // Validate and sanitize fields.
+    body('title', 'Title must not be empty.').isLength({ min: 1 }),
+    body('body', 'The actually post is required, buddy')
+        .trim()
+        .isLength({ min: 1 }),
 
-        // Process request after validation and sanitization.
-        (req, res, next) => {
-            // Extract the validation errors from a request.
-            const errors = validationResult(req);
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
 
-            // Create a message object with escaped and trimmed data.
-            const post = new Post({
-                title: req.body.title,
-                body_text: req.body.body,
-                date: new Date(),
-                author: '62281f586618ead14f6ea580',
-                stage: 'draft',
-                labels: [],
+        // Create a message object with escaped and trimmed data.
+        const post = new Post({
+            title: req.body.title,
+            body_text: req.body.body,
+            date: new Date(),
+            author: '62281f586618ead14f6ea580',
+            stage: 'draft',
+            labels: [],
+        });
+
+        if (!errors.isEmpty()) {
+            //Successful, so render
+            res.json(errors);
+            return;
+        } else {
+            // Data from form is valid. Save item.
+            post.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
             });
-
-            if (!errors.isEmpty()) {
-                //Successful, so render
-                res.json(errors);
-                return;
-            } else {
-                // Data from form is valid. Save item.
-                post.save(function (err) {
-                    if (err) {
-                        return next(err);
-                    }
-                });
-                res.status(200).json(post);
-            }
-        },
-    ];
+            res.status(200).json(post);
+        }
+    },
+];
 
 /* Update Post at /post/:id/update/ */
 exports.post_update_post = [
